2c001fd057bf182ee541229ebd2d664eb79d96e0
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2023-05-06 22:23:56 +0530

    added github link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import ListTodo from "./components/ListTodo";
+import { VscGithub } from "react-icons/vsc";
 
 function App() {
   return (
     <>
       <section className="flex flex-col gap-10 justify-center items-center">
+        <a
+          href="https://github.com/lokeshkavisth"
+          target="_blank"
+          rel="noreferrer"
+          className="text-3xl"
+          title="github.com/lokeshkavisth"
+        >
+          <VscGithub />
+        </a>
         <AddTodo />
         <ListTodo />
       </section>
